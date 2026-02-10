cc199855df30aa144ed397c166511ef986662180
creo.cons <creo.cons@bene.it>
2026-02-10 22:52:25 +0100

    second

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,6 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.postgresql:postgresql")
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
-    implementation("me.paulschwarz:spring-dotenv:4.0.0")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
